b503382b420e85f8c6436a6b0eed4b3b09988f51
Nilutpol Kashyap <47447024+nilutpolkashyap@users.noreply.github.com>
2022-03-04 23:42:12 +0530

    Create esp32_bluetooth.py

---
+++ esp32_bluetooth/esp32_bluetooth.py
@@ -0,0 +1,48 @@
+import bluetooth
+
+#bDetect all Bluetooth devices and Create an array with all the MAC addresses
+print("Searching for devices...")
+nearby_devices = bluetooth.discover_devices(lookup_names=True)
+
+num = 0
+
+#Run through all the devices found and list their name
+print("Select your device by entering its coresponding number...")
+for i in nearby_devices:
+    num+=1
+    print(str(num) + " : " + i[1])
+   
+#Allow the user to select their Arduino
+selection = int(input("> ")) - 1
+bd_addr = nearby_devices[selection]
+port = 1
+
+# print("You have selected " + bluetooth.lookup_name(nearby_devices[selection]))
+print("You have selected : ", nearby_devices[selection])
+
+# Connect to bluetooth address and port
+sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+sock.connect((bd_addr[0], port))
+
+data = "L"
+sock.send(data)
+
+data = "Z"
+# sock.send(data)
+
+# data1 = sock.recv(1024)
+# print(data1)
+# Print out appearsto be those of Serial.println and not bluetooth.println
+
+while True:
+    # sock.send(data)
+    data1 = sock.recv(1024)
+    print(data1)
+
+   
+sock.getsockname()
+# sock.getpeername()
+
+
+
+sock.close()
